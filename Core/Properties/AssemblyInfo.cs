57350ee998e9b4fc1b177097ad19b75e079c14d1
Chad Lee <chad.lee@archoninfosys.com>
2014-10-08 13:56:06 -0500

    nuget doesn't like ampersands

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Archon Web API Framework")]
-[assembly: AssemblyDescription("Extension & helper methods that make working with Asp.Net Web API easier")]
+[assembly: AssemblyDescription("Extension and helper methods that make working with Asp.Net Web API easier")]
