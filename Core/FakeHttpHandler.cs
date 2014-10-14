b52ccf569b3a3124c8d4a71fd0f244d7ccefb4a1
Chad Lee <chad.lee@archoninfosys.com>
2014-10-13 19:06:29 -0500

    Adding FakeHttpHandler

---
+++ Core/FakeHttpHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+
+namespace Archon.WebApi
+{
+	public class FakeHttpHandler : HttpMessageHandler
+	{
+		public Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> Action { get; set; }
+
+		protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+
+			if (Action == null)
+				return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK));
+
+			return Action(request, cancellationToken);
+		}
+	}
+}
