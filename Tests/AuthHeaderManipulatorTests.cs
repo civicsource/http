ccbbc34f2e435a4b3b22b64e0b4df435be166583
Chad Lee <chad.lee@archoninfosys.com>
2014-10-22 15:13:02 -0500

    Adding AssertHasBeenCalled method to FakeHttpHandler

--- Tests/AuthHeaderManipulatorTests.cs
+++ Tests/AuthHeaderManipulatorTests.cs
@@ -21,36 +21,29 @@ namespace Archon.WebApi.Tests
 		[Fact]
 		public void can_parse_auth_parameter_to_bearer_token_header()
 		{
-			bool ran = false;
-
 			handler.Action = (req, c) =>
 			{
 				Assert.NotNull(req.Headers.Authorization);
 				Assert.Equal("Bearer", req.Headers.Authorization.Scheme);
 				Assert.Equal("my-token", req.Headers.Authorization.Parameter);
-				ran = true;
-
 				return Task.FromResult(req.CreateResponse(HttpStatusCode.OK));
 			};
 
 			var result = client.GetAsync("http://example.com/?auth=my-token").Result;
-			Assert.True(ran, "Did not run fake test handler");
+			handler.AssertHasBeenCalled();
 		}
 
 		[Fact]
 		public void can_not_parse_other_parameters_as_bearer_token_header()
 		{
-			bool ran = false;
-
 			handler.Action = (req, c) =>
 			{
 				Assert.Null(req.Headers.Authorization);
-				ran = true;
 				return Task.FromResult(req.CreateResponse(HttpStatusCode.OK));
 			};
 
 			var result = client.GetAsync("http://example.com/?auths=my-token").Result;
-			Assert.True(ran, "Did not run fake test handler");
+			handler.AssertHasBeenCalled();
 		}
 	}
 }
