7f487f2623b7ac9640ca96607e2979fc28d00e99
Brian Oline <brian.oline@archoninfosys.com>
2015-02-25 13:55:02 -0600

    Adding CsvStringArrayConverterAttribute

---
+++ Core/CsvStringArrayConverterAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Net.Http;
+
+namespace Archon.WebApi
+{
+	public class CsvStringArrayConverterAttribute : ActionFilterAttribute
+	{
+		readonly string name;
+
+		public CsvStringArrayConverterAttribute(string name)
+		{
+			this.name = name;
+		}
+
+		public override void OnActionExecuting(HttpActionContext context)
+		{
+			string csv = ExtractCommaSeparatedValue(context);
+
+			if (!String.IsNullOrWhiteSpace(csv))
+			{
+				context.ActionArguments[name] = csv.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray();
+			}
+			else
+			{
+				context.ActionArguments[name] = new string[0];
+			}
+		}
+
+		string ExtractCommaSeparatedValue(HttpActionContext context)
+		{
+			if (!context.ActionArguments.ContainsKey(name))
+				return null;
+
+			if (context.ControllerContext.RouteData.Values.ContainsKey(this.name))
+				return (string)context.ControllerContext.RouteData.Values[this.name];
+
+			var qs = context.ControllerContext.Request.RequestUri.ParseQueryString();
+			return qs[name];
+		}
+	}
+}
