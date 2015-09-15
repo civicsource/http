ddada153fa818232ca3bdb30004943d10b5ff217
Chad Lee <chad.lee@archoninfosys.com>
2015-09-15 15:21:52 -0500

    Adding MethodConstrainedRoute feature
    See
    http://stackoverflow.com/questions/23094584/multiple-controller-types-with-same-route-prefix-asp-net-web-api

---
+++ Core/MethodConstraint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http.Routing;
+
+namespace Archon.WebApi
+{
+	/// <remarks>
+	/// http://stackoverflow.com/a/23097445/316108
+	/// </remarks>
+	class MethodConstraint : IHttpRouteConstraint
+	{
+		public HttpMethod Method { get; private set; }
+
+		public MethodConstraint(HttpMethod method)
+		{
+			Method = method;
+		}
+
+		public bool Match(HttpRequestMessage request, IHttpRoute route, string parameterName, IDictionary<string, object> values, HttpRouteDirection routeDirection)
+		{
+			return request.Method == Method;
+		}
+	}
+}
---
+++ Core/MethodRouteAttributes.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+
+namespace Archon.WebApi
+{
+	//http://stackoverflow.com/a/23097445/316108
+
+	public class GetRouteAttribute : MethodConstraintedRouteAttribute
+	{
+		public GetRouteAttribute(string template) : base(template, HttpMethod.Get) { }
+	}
+
+	public class HeadRouteAttribute : MethodConstraintedRouteAttribute
+	{
+		public HeadRouteAttribute(string template) : base(template, HttpMethod.Head) { }
+	}
+
+	public class PostRouteAttribute : MethodConstraintedRouteAttribute
+	{
+		public PostRouteAttribute(string template) : base(template, HttpMethod.Post) { }
+	}
+
+	public class PatchRouteAttribute : MethodConstraintedRouteAttribute
+	{
+		public PatchRouteAttribute(string template) : base(template, new HttpMethod("PATCH")) { }
+	}
+
+	public class PutRouteAttribute : MethodConstraintedRouteAttribute
+	{
+		public PutRouteAttribute(string template) : base(template, HttpMethod.Put) { }
+	}
+
+	public class DeleteRouteAttribute : MethodConstraintedRouteAttribute
+	{
+		public DeleteRouteAttribute(string template) : base(template, HttpMethod.Delete) { }
+	}
+}
