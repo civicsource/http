807d8e9ad555151427282f2d7e3134e53ca4567a
Chad Lee <chad.lee@archoninfosys.com>
2016-02-26 14:33:43 -0600

    Update copyright year

--- AssemblyInfoCommon.cs
+++ AssemblyInfoCommon.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Archon Framework Library™")]
 [assembly: AssemblyCompany("Archon Information Systems, LLC")]
-[assembly: AssemblyCopyright("Copyright © 2008-2013 Archon Information Systems, LLC")]
+[assembly: AssemblyCopyright("Copyright © 2008-2016 Archon Information Systems, LLC")]
 [assembly: ComVisible(false)]
