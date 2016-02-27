e1610a4e2d06712ed740dd6712b7ce2a611c87d0
Chad Lee <git@chadly.net>
2016-02-27 09:59:16 -0600

    Removing extension methods that no longer apply

--- Archon.WebApi/RequestExtensions.cs
+++ Archon.WebApi/RequestExtensions.cs
@@ -7,21 +7,6 @@ namespace Archon.WebApi
 {
 	public static class RequestExtensions
 	{
-		public static HttpResponseMessage CreateNotFoundResponse(this HttpRequestMessage request, string name, Guid id)
-		{
-			return request.CreateErrorResponse(HttpStatusCode.NotFound, BuildNotFoundMessage(name, id.ToString()));
-		}
-
-		public static HttpResponseMessage CreateNotFoundResponse(this HttpRequestMessage request, string name, string identifier)
-		{
-			return request.CreateErrorResponse(HttpStatusCode.NotFound, BuildNotFoundMessage(name, identifier));
-		}
-
-		static string BuildNotFoundMessage(string name, string identifier)
-		{
-			return String.Format("Could not find {0} with identifier '{1}'.", name.ToLower(), identifier);
-		}
-
 		public static HttpRequestMessage WithJsonContent<T>(this HttpRequestMessage request, T content)
 		{
 			request.Content = new ObjectContent<T>(content, new JsonMediaTypeFormatter());
