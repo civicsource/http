bc9f366b187f879bff70ba9897bba5f1c63dee09
Daniel Tullemans <daniel.tullemans@civicsource.com>
2018-03-06 17:35:11 -0600

    Add classes for GZip compression
    CS-16240

---
+++ src/GZipCompressPostHandler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Archon.Http
+{
+	/// <summary>
+	/// GZip-compresses requests to save bytes over the wire
+	/// </summary>
+	public sealed class GZipCompressingHandler : DelegatingHandler
+	{
+		private readonly ImmutableHashSet<HttpMethod> verbs;
+
+		/// <inheritdoc cref="DelegatingHandler"/>
+		/// <param name="verbsToCompress">A list of HTTP verbs to compress. Generally, only <see cref="HttpMethod.Post"/> and <see cref="HttpMethod.Put"/> are particularly useful.</param>
+		public GZipCompressingHandler(HttpMessageHandler innerHandler, params HttpMethod[] verbsToCompress)
+			: base(innerHandler)
+		{
+			if (verbsToCompress.Length == 0)
+				throw new ArgumentException("Must specify at least one HTTP verb to compress", nameof(verbsToCompress));
+
+			verbs = verbsToCompress.ToImmutableHashSet();
+		}
+
+		protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			if (verbs.Contains(request.Method))
+				request.Content = new GZipHttpContent(request.Content);
+
+			return base.SendAsync(request, cancellationToken);
+		}
+	}
+
+	public sealed class GZipHttpContent : HttpContent
+	{
+		private readonly HttpContent originalContent;
+
+		public GZipHttpContent(HttpContent content)
+		{
+			originalContent = content;
+
+			foreach (var header in originalContent.Headers)
+			{
+				Headers.TryAddWithoutValidation(header.Key, header.Value);
+			}
+
+			Headers.ContentEncoding.Add("gzip");
+		}
+
+		protected override async Task SerializeToStreamAsync(Stream stream, TransportContext context)
+		{
+			using (var gzip = new GZipStream(stream, CompressionLevel.Fastest, true))
+			{
+				await originalContent.CopyToAsync(gzip);
+			}
+		}
+
+		protected override bool TryComputeLength(out long length)
+		{
+			length = -1;
+
+			return false;
+		}
+	}
+}
---
+++ src/GZipResourceFilter.cs
@@ -0,0 +1,25 @@
+﻿using System.IO.Compression;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+
+namespace Archon.Http
+{
+	// TODO: Separate the server and client helpers in this project so that our web clients don't need to import frickin' ASP.NET
+	
+	/// <summary>
+	/// A filter that decompresses GZip content from HTTP requests specified as <c>Content-Encoding: gzip</c>
+	/// </summary>
+	public class GZipResourceFilter : IAsyncResourceFilter
+	{
+		public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+		{
+			if (context.HttpContext.Request.Headers.TryGetValue("Content-Encoding", out StringValues values)
+			    && values.Count == 1 
+			    && (values[0] == "gzip" || values[0] == "x-gzip"))
+				context.HttpContext.Request.Body = new GZipStream(context.HttpContext.Request.Body, CompressionMode.Decompress, false);
+
+			await next();
+		}
+	}
+}
